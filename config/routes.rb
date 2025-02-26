039761317ded633ffb6af0e354b058664a6d5da0
Jaren Garman <jarengarman@protonmail.com>
2025-02-26 16:55:14 -0500

    feat: Add route for merchant items

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
 
   get "/api/v1/items", to: "api/v1/items#index"
   get "/api/v1/merchants", to: "api/v1/merchants#index"
+  get "/api/v1/merchants/:id/items", to: "api/v1/merchant/items#index"
   get "/api/v1/items/:id", to: "api/v1/items#show"
   get "/api/v1/merchants/:id", to: "api/v1/merchants#show"
   post "/api/v1/items", to: "api/v1/items#create"
