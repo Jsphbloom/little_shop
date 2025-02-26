90b7d7a179a3f6a5c8b25fe1370330a010af2126
Jaren Garman <jarengarman@protonmail.com>
2025-02-25 20:19:46 -0500

    test: Add tests for item update action

--- spec/api/v1/items_controller_spec.rb
+++ spec/api/v1/items_controller_spec.rb
@@ -12,9 +12,9 @@ describe "Items API", type: :request do
   describe "create" do
     it "can create a new item" do
       item_params = {
-        name: "value1",
-        description: "value2",
-        unit_price: 100.99,
+        name: Faker::Commerce.product_name,
+        description: Faker::Commerce.material,
+        unit_price: Faker::Commerce.price,
         merchant_id: Merchant.last.id
       }
       headers = {"CONTENT_TYPE" => "application/json"}
@@ -41,6 +41,41 @@ describe "Items API", type: :request do
     end
   end
 
+  describe "update" do
+    it "can update an existing item" do
+      item = create(:item)
+
+      item_params = {
+        name: Faker::Commerce.product_name,
+        description: Faker::Commerce.material,
+        unit_price: Faker::Commerce.price,
+        merchant_id: Merchant.all.sample.id
+      }
+      headers = {"CONTENT_TYPE" => "application/json"}
+
+      patch "/api/v1/items/#{item.id}", headers: headers, params: JSON.generate(item: item_params)
+
+      expect(response).to be_successful
+
+      item = Item.find(item.id)
+
+      expect(item).to have_attributes(item_params)
+
+      response_data = parsed_response
+
+      expect(response_data[:data]).to include(
+        id: item.id.to_s,
+        type: "item"
+      )
+
+      expect(response_data[:data][:attributes]).to include(item_params)
+      expect(item.name).to eq(item_params[:name])
+      expect(item.description).to eq(item_params[:description])
+      expect(item.unit_price).to eq(item_params[:unit_price])
+      expect(item.merchant_id).to eq(item_params[:merchant_id])
+    end
+  end
+
   describe "sad paths" do
     it "will gracefully handle if name isn't provided" do
       post "/api/v1/items", headers: headers, params: JSON.generate({})
@@ -57,6 +92,15 @@ describe "Items API", type: :request do
       expect(response_data).to have_key(:errors)
       expect(response_data[:errors]).to be_a(Array)
       expect(response_data[:errors].first).to eq("422")
+    end
+
+    it "will gracefully handle update if params aren't provided" do
+      item = create(:item)
+
+      patch "/api/v1/items/#{item.id}", headers: headers, params: JSON.generate(item: {})
+
+      expect(response).not_to be_successful
+      expect(response.status).to eq(400).or eq(404)
     end
   end
 end
