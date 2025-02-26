290b61743195095ed5d8888884c83df991731b4a
Donte Handy <donte.handy@gmail.com>
2025-02-25 22:28:10 -0400

    test: Add comments to request specs for items and merchants

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -1,4 +1,5 @@
 require "rails_helper"
+#test by running rspec spec/requests/api/v1/items_request_spec.rb
 
 RSpec.describe "Items API", type: :request do
   describe "DELETE /api/v1/items/:id" do
@@ -21,3 +22,5 @@ RSpec.describe "Items API", type: :request do
     end
   end
 end
+
+
