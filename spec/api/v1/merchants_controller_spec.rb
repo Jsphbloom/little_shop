60200f0e9044dba29c037600e5906964826e40ae
Jaren Garman <jarengarman@protonmail.com>
2025-02-25 19:37:23 -0500

    test: Add tests for merchant update

--- spec/api/v1/merchants_controller_spec.rb
+++ spec/api/v1/merchants_controller_spec.rb
@@ -7,7 +7,7 @@ describe "Merchants API", type: :request do
 
   describe "create" do
     it "can create a new merchant" do
-      merchant_params = {name: "merchant_name"}
+      merchant_params = {name: Faker::Commerce.vendor}
       headers = {"CONTENT_TYPE" => "application/json"}
 
       post "/api/v1/merchants", headers: headers, params: JSON.generate(merchant: merchant_params)
@@ -29,9 +29,53 @@ describe "Merchants API", type: :request do
     end
   end
 
+  describe "update" do
+    it "can update an existing merchant" do
+      merchant = create(:merchant)
+
+      merchant_params = {name: Faker::Commerce.vendor}
+      headers = {"CONTENT_TYPE" => "application/json"}
+
+      patch "/api/v1/merchants/#{merchant.id}", headers: headers, params: JSON.generate(merchant: merchant_params)
+
+      expect(response).to be_successful
+
+      expect(created_merchant).to have_attributes(merchant_params)
+
+      response_data = parsed_response
+
+      expect(response_data[:data]).to include(
+        id: created_merchant.id.to_s,
+        type: "merchant"
+      )
+
+      expect(response_data[:data][:attributes]).to include(merchant_params)
+      expect(merchant.name).to eq(merchant_params[:name])
+    end
+  end
+
   describe "sad paths" do
-    it "will gracefully handle if name isn't provided" do
-      post "/api/v1/merchants", headers: headers, params: JSON.generate({})
+    it "will gracefully handle create if name isn't provided" do
+      post "/api/v1/merchants", headers: headers, params: JSON.generate(merchant: {})
+
+      expect(response).not_to be_successful
+      expect(response.status).to eq(422)
+
+      response_data = parsed_response
+
+      expect(response_data).to have_key(:message)
+      expect(response_data[:message]).to be_a(String)
+      expect(response_data[:message]).to eq("param is missing or the value is empty: merchant")
+
+      expect(response_data).to have_key(:errors)
+      expect(response_data[:errors]).to be_a(Array)
+      expect(response_data[:errors].first).to eq("422")
+    end
+
+    it "will gracefully handle update if name isn't provided" do
+      merchant = create(:merchant)
+
+      patch "/api/v1/merchants/#{merchant.id}", headers: headers, params: JSON.generate(merchant: {})
 
       expect(response).not_to be_successful
       expect(response.status).to eq(422)
