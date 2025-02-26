481542e4be998388014eb0e3411035348fdb83c2
Jaren Garman <jarengarman@protonmail.com>
2025-02-25 20:21:56 -0500

    feat: Add route for item update action

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,6 @@ Rails.application.routes.draw do
 
   post "/api/v1/items", to: "api/v1/items#create"
   post "/api/v1/merchants", to: "api/v1/merchants#create"
+  patch "/api/v1/items/:id", to: "api/v1/items#update"
   patch "/api/v1/merchants/:id", to: "api/v1/merchants#update"
 end
