3f93666ddfe279c2e4967d844f272e130b8f334f
Donte Handy <donte.handy@gmail.com>
2025-02-27 06:58:57 -0400

    refactor: Enhance comments in Merchant model for customers association clarity

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -4,4 +4,8 @@ class Merchant < ApplicationRecord
   has_many :items, dependent: :destroy
   # The customers association leverages invoices to return unique customers.
   has_many :customers, -> { distinct }, through: :invoices
+  #explanation of the above line:
+  #The customers association leverages invoices to return unique customers.
+  #The through: :invoices option tells Rails to use the invoices association to find the customers.
+  #The -> { distinct } lambda tells Rails to return only unique customers.
 end
