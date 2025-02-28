b48b233a6e901efe407892f4e44a044e944a8a70
Donte Handy <donte.handy@gmail.com>
2025-02-28 17:43:30 -0400

    Refactor tests to use FactoryBot for dynamic record creation

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -4,18 +4,18 @@ require "rails_helper"
 RSpec.describe "Merchants API", type: :request do
   before do
     Merchant.destroy_all
-    # Create dummy merchants with "s" prefix as instance variables
-    @sMerchant1 = Merchant.create!(name: "Dummy Merchant 1")
-    @sMerchant2 = Merchant.create!(name: "Dummy Merchant 2")
-    @sMerchant3 = Merchant.create!(name: "Dummy Merchant 3")
+    # Converted manual static creation to dynamic FactoryBot records.
+    @sMerchant1 = create(:merchant, name: Faker::Company.unique.name)
+    @sMerchant2 = create(:merchant, name: Faker::Company.unique.name)
+    @sMerchant3 = create(:merchant, name: Faker::Company.unique.name)
   end
 
   def parsed_response
     JSON.parse(response.body, symbolize_names: true)
   end
 
-  describe "GET /api/v1/merchants" do 
-    it "can return a single merchant by id" do 
+  describe "GET /api/v1/merchants" do
+    it "can return a single merchant by id" do
       merchant = create(:merchant)
 
       get "/api/v1/merchants/#{merchant.id}"
@@ -27,7 +27,7 @@ RSpec.describe "Merchants API", type: :request do
 
       expect(response_data[:data]).to include(
         id: merchant.id.to_s,
-        type: "merchant" 
+        type: "merchant"
       )
 
       expect(response_data[:data][:attributes]).to include(
@@ -44,41 +44,41 @@ RSpec.describe "Merchants API", type: :request do
       expect(response_data[:error]).to eq("Merchant not found")
     end
   end
-  
+
   describe "GET /api/v1/merchants" do
     it "can return an index of merchants" do
       get "/api/v1/merchants"
       expect(response).to be_successful
-      
+
       response_data = parsed_response
       expect(response_data[:data].count).to eq(3)
-      expect(response_data[:data][0][:attributes][:name]).to eq("Dummy Merchant 1")
-      expect(response_data[:data][1][:attributes][:name]).to eq("Dummy Merchant 2")
-      expect(response_data[:data][2][:attributes][:name]).to eq("Dummy Merchant 3")
+      expect(response_data[:data][0][:attributes][:name]).to eq(@sMerchant1.name)
+      expect(response_data[:data][1][:attributes][:name]).to eq(@sMerchant2.name)
+      expect(response_data[:data][2][:attributes][:name]).to eq(@sMerchant3.name)
     end
 
     it "can sort merchants by oldest to newest" do
       get "/api/v1/merchants?sort=desc"
       expect(response).to be_successful
-      
+
       response_data = parsed_response
 
       expect(response_data[:data].count).to eq(3)
-      expect(response_data[:data][0][:attributes][:name]).to eq("Dummy Merchant 3")
-      expect(response_data[:data][1][:attributes][:name]).to eq("Dummy Merchant 2")
-      expect(response_data[:data][2][:attributes][:name]).to eq("Dummy Merchant 1")
+      expect(response_data[:data][0][:attributes][:name]).to eq(@sMerchant3.name)
+      expect(response_data[:data][1][:attributes][:name]).to eq(@sMerchant2.name)
+      expect(response_data[:data][2][:attributes][:name]).to eq(@sMerchant1.name)
     end
 
     it "can sort merchants by newest to oldest" do
       get "/api/v1/merchants?sort=asc"
       expect(response).to be_successful
-      
+
       response_data = parsed_response
 
       expect(response_data[:data].count).to eq(3)
-      expect(response_data[:data][0][:attributes][:name]).to eq("Dummy Merchant 1")
-      expect(response_data[:data][1][:attributes][:name]).to eq("Dummy Merchant 2")
-      expect(response_data[:data][2][:attributes][:name]).to eq("Dummy Merchant 3")
+      expect(response_data[:data][0][:attributes][:name]).to eq(@sMerchant1.name)
+      expect(response_data[:data][1][:attributes][:name]).to eq(@sMerchant2.name)
+      expect(response_data[:data][2][:attributes][:name]).to eq(@sMerchant3.name)
     end
   end
 
