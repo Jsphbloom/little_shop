bb2b1ca1524accaa7230828a5b40463896776d19
Jaren Garman <jarengarman@protonmail.com>
2025-02-28 19:41:51 -0500

    revert: Remove unused error handling

--- app/controllers/api/v1/items/merchant_controller.rb
+++ app/controllers/api/v1/items/merchant_controller.rb
@@ -1,5 +1,4 @@
 class Api::V1::Items::MerchantController < ApplicationController
-  rescue_from ActionController::ParameterMissing, with: :unprocessable_entity_response
   rescue_from ActiveRecord::RecordNotFound, with: :not_found_response
   def index
     merchant = Item.find(params[:id]).merchant
@@ -7,10 +6,6 @@ class Api::V1::Items::MerchantController < ApplicationController
   end
 
   private
-
-  def unprocessable_entity_response(e)
-    render json: ErrorSerializer.format_error(e, "422"), status: :unprocessable_entity
-  end
 
   def not_found_response(e)
     render json: ErrorSerializer.format_error(e, "404"), status: :not_found
