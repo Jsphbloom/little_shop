afcc693ca6dd12d1f0d70dcf97d432469f938e86
Jaren Garman <jarengarman@protonmail.com>
2025-02-28 20:44:50 -0500

    test: Refactor and reorganize main tests, add sad path test for string id

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -1,35 +1,107 @@
 require "rails_helper"
 RSpec.describe "Items API", type: :request do
-  before do
-    Merchant.destroy_all
-    Item.destroy_all
-    @sMerchant1 = create(:merchant, name: Faker::Company.unique.name)
-    @sMerchant2 = create(:merchant, name: Faker::Company.unique.name)
-    @sMerchant3 = create(:merchant, name: Faker::Company.unique.name)
-    @sItem1 = create(:item,
-      name: Faker::Commerce.product_name,
-      description: Faker::Commerce.material,
-      unit_price: Faker::Commerce.price,
-      merchant: @sMerchant1)
-    @sItem2 = create(:item,
-      name: Faker::Commerce.product_name,
-      description: Faker::Commerce.material,
-      unit_price: Faker::Commerce.price,
-      merchant: @sMerchant2)
-    @sItem3 = create(:item,
-      name: Faker::Commerce.product_name,
-      description: Faker::Commerce.material,
-      unit_price: Faker::Commerce.price,
-      merchant: @sMerchant3)
-  end
-
   def parsed_response
     JSON.parse(response.body, symbolize_names: true)
+  end
+
+  describe "GET /api/v1/items" do
+    it "can return an index of items" do
+      create_list(:item, 3)
+      get "/api/v1/items"
+
+      expect(response).to be_successful
+      expect(response.status).to eq(200)
+
+      response_data = parsed_response
+
+      expect(response_data).to have_key(:data)
+      expect(response_data[:data]).to be_an(Array)
+
+      response_items = response_data[:data]
+
+      expect(response_items.length).to eq(3)
+
+      response_items.each do |item|
+        expect(item).to have_key(:id)
+        expect(item[:id]).to be_a(String)
+
+        expect(item).to have_key(:type)
+        expect(item[:type]).to eq("item")
+
+        expect(item).to have_key(:attributes)
+        expect(item[:attributes]).to be_a(Hash)
+
+        attributes = item[:attributes]
+
+        expect(attributes).to have_key(:name)
+        expect(attributes[:name]).to be_a(String)
+
+        expect(attributes).to have_key(:description)
+        expect(attributes[:description]).to be_a(String)
+
+        expect(attributes).to have_key(:unit_price)
+        expect(attributes[:unit_price]).to be_a(Float)
+
+        expect(attributes).to have_key(:merchant_id)
+        expect(attributes[:merchant_id]).to be_an(Integer)
+      end
+    end
+
+    it "can sort items by price" do
+      item1 = create(:item, unit_price: 2.0)
+      item2 = create(:item, unit_price: 3.0)
+      item3 = create(:item, unit_price: 4.0)
+      item4 = create(:item, unit_price: 1.0)
+      get "/api/v1/items?sorted=price"
+
+      expect(response).to be_successful
+      expect(response.status).to eq(200)
+
+      response_data = parsed_response
+
+      expect(response_data).to have_key(:data)
+      expect(response_data[:data]).to be_an(Array)
+
+      response_items = response_data[:data]
+
+      expect(response_items.length).to eq(4)
+
+      response_items.each do |item|
+        expect(item).to have_key(:id)
+        expect(item[:id]).to be_a(String)
+
+        expect(item).to have_key(:type)
+        expect(item[:type]).to eq("item")
+
+        expect(item).to have_key(:attributes)
+        expect(item[:attributes]).to be_a(Hash)
+
+        attributes = item[:attributes]
+
+        expect(attributes).to have_key(:name)
+        expect(attributes[:name]).to be_a(String)
+
+        expect(attributes).to have_key(:description)
+        expect(attributes[:description]).to be_a(String)
+
+        expect(attributes).to have_key(:unit_price)
+        expect(attributes[:unit_price]).to be_a(Float)
+
+        expect(attributes).to have_key(:merchant_id)
+        expect(attributes[:merchant_id]).to be_an(Integer)
+      end
+
+      expect(response_data[:data][0][:attributes][:name]).to eq(item4.name)
+      expect(response_data[:data][1][:attributes][:name]).to eq(item1.name)
+      expect(response_data[:data][2][:attributes][:name]).to eq(item2.name)
+      expect(response_data[:data][3][:attributes][:name]).to eq(item3.name)
+    end
   end
 
   describe "GET /api/v1/items/:id" do
     it "can fetch a single existing item by id" do
       item = create(:item)
+
       get "/api/v1/items/#{item.id}"
 
       expect(response).to be_successful
@@ -42,61 +114,30 @@ RSpec.describe "Items API", type: :request do
         type: "item"
       )
 
-      expect(response_data[:data][:attributes]).to include(
+      expect(response_data).to have_key(:data)
+      expect(response_data[:data]).to be_a(Hash)
+
+      response_item = response_data[:data]
+
+      expect(response_item).to have_key(:id)
+      expect(response_item[:id]).to be_a(String)
+
+      expect(response_item).to have_key(:type)
+      expect(response_item[:type]).to eq("item")
+
+      expect(response_item).to have_key(:attributes)
+      expect(response_item[:attributes]).to be_a(Hash)
+      expect(response_item[:attributes]).to include(
         name: item.name,
         description: item.description,
         unit_price: item.unit_price
       )
     end
-
-    it "returns a 404 if the item is not found" do
-      get "/api/v1/items/0"
-
-      expect(response).to have_http_status(:not_found)
-
-      response_data = parsed_response
-      expect(response_data[:message]).to eq("Couldn't find Item with 'id'=0")
-    end
-  end
-
-  describe "GET /api/v1/items" do
-    it "can return an index of items" do
-      get "/api/v1/items"
-
-      expect(response).to be_successful
-
-      response_data = parsed_response
-
-      expect(response_data[:data].count).to eq(3)
-      expect(response_data[:data][0][:attributes][:name]).to eq(@sItem1.name)
-      expect(response_data[:data][0][:attributes][:merchant_id]).to eq(@sMerchant1.id)
-      expect(response_data[:data][1][:attributes][:name]).to eq(@sItem2.name)
-      expect(response_data[:data][1][:attributes][:merchant_id]).to eq(@sMerchant2.id)
-      expect(response_data[:data][2][:attributes][:name]).to eq(@sItem3.name)
-      expect(response_data[:data][2][:attributes][:merchant_id]).to eq(@sMerchant3.id)
-    end
-
-    it "can sort items by price" do
-      @sItem4 = Item.create!(
-        name: "Dummy Item 4",
-        description: "Dummy description 4",
-        unit_price: 1.0,
-        merchant: @sMerchant1
-      )
-      get "/api/v1/items?sorted=price"
-      expect(response).to be_successful
-
-      response_data = parsed_response
-
-      expect(response_data[:data].count).to eq(4)
-      expect(response_data[:data][0][:attributes][:name]).to eq("Dummy Item 4")
-      expect(response_data[:data][1][:attributes][:name]).to eq("Dummy Item 1")
-      expect(response_data[:data][2][:attributes][:name]).to eq("Dummy Item 2")
-    end
   end
 
   describe "create" do
     it "POST /api/v1/items/" do
+      create(:merchant)
       item_params = {
         name: Faker::Commerce.product_name,
         description: Faker::Commerce.material,
@@ -108,6 +149,7 @@ RSpec.describe "Items API", type: :request do
       post "/api/v1/items", headers: headers, params: JSON.generate(item: item_params)
 
       expect(response).to be_successful
+      expect(response.status).to eq(201)
 
       created_item = Item.last
 
@@ -120,6 +162,19 @@ RSpec.describe "Items API", type: :request do
         type: "item"
       )
 
+      expect(response_data).to have_key(:data)
+      expect(response_data[:data]).to be_a(Hash)
+
+      response_item = response_data[:data]
+
+      expect(response_item).to have_key(:id)
+      expect(response_item[:id]).to be_a(String)
+
+      expect(response_item).to have_key(:type)
+      expect(response_item[:type]).to eq("item")
+
+      expect(response_item).to have_key(:attributes)
+      expect(response_item[:attributes]).to be_a(Hash)
       expect(response_data[:data][:attributes]).to include(item_params)
     end
   end
@@ -139,6 +194,7 @@ RSpec.describe "Items API", type: :request do
       put "/api/v1/items/#{item.id}", headers: headers, params: JSON.generate(item: item_params)
 
       expect(response).to be_successful
+      expect(response.status).to eq(200)
 
       item = Item.find(item.id)
 
@@ -151,6 +207,19 @@ RSpec.describe "Items API", type: :request do
         type: "item"
       )
 
+      expect(response_data).to have_key(:data)
+      expect(response_data[:data]).to be_a(Hash)
+
+      response_item = response_data[:data]
+
+      expect(response_item).to have_key(:id)
+      expect(response_item[:id]).to be_a(String)
+
+      expect(response_item).to have_key(:type)
+      expect(response_item[:type]).to eq("item")
+
+      expect(response_item).to have_key(:attributes)
+      expect(response_item[:attributes]).to be_a(Hash)
       expect(response_data[:data][:attributes]).to include(item_params)
       expect(item.name).to eq(item_params[:name])
       expect(item.description).to eq(item_params[:description])
@@ -161,18 +230,42 @@ RSpec.describe "Items API", type: :request do
 
   describe "DELETE /api/v1/items/:id" do
     it "deletes an existing item created in the before block" do
-      delete "/api/v1/items/#{@sItem1.id}"
-      expect(response).to have_http_status(:no_content)
-      expect(Item.find_by(id: @sItem1.id)).to be_nil
-    end
+      item = create(:item)
 
-    it "returns not found when deleting a non-existent item" do
-      delete "/api/v1/items/0"
-      expect(response).to have_http_status(:not_found)
+      delete "/api/v1/items/#{item.id}"
+
+      expect(response).to be_successful
+      expect(response.status).to eq(204)
+
+      expect(Item.find_by(id: item.id)).to be_nil
     end
   end
 
   describe "sad paths" do
+    it "will gracefully handle a nonexistent item id" do
+      get "/api/v1/items/8923987297"
+
+      expect(response).not_to be_successful
+      expect(response.status).to eq(404)
+
+      response_data = parsed_response
+
+      expect(response_data[:errors]).to eq(["404"])
+      expect(response_data[:message]).to eq("Couldn't find Item with 'id'=8923987297")
+    end
+
+    it "will gracefully handle an invalid item id" do
+      get "/api/v1/items/string-instead-of-integer"
+
+      expect(response).not_to be_successful
+      expect(response.status).to eq(404)
+
+      response_data = parsed_response
+
+      expect(response_data[:errors]).to eq(["404"])
+      expect(response_data[:message]).to eq("Couldn't find Item with 'id'=string-instead-of-integer")
+    end
+
     it "will gracefully handle if name isn't provided" do
       post "/api/v1/items", headers: headers, params: JSON.generate({})
 
@@ -206,6 +299,11 @@ RSpec.describe "Items API", type: :request do
 
       expect(response).not_to be_successful
       expect(response.status).to eq(422)
+    end
+
+    it "returns not found when deleting a non-existent item" do
+      delete "/api/v1/items/0"
+      expect(response).to have_http_status(:not_found)
     end
   end
 end
