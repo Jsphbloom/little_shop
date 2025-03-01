492d50bf420e312b481243b7b0f7daeb348a290a
Donte Handy <donte.handy@gmail.com>
2025-03-01 09:01:38 -0400

    style: Add comments to find and find_all routes for clarity

--- config/routes.rb
+++ config/routes.rb
@@ -15,10 +15,10 @@ Rails.application.routes.draw do
   get "/api/v1/items/:id", to: "api/v1/items#show"
   get "/api/v1/merchants/:id", to: "api/v1/merchants#show"
   get "/api/v1/merchants/:merchant_id/customers", to: "api/v1/merchant/customers#index"
-  get "/api/v1/items/find", to: "api/v1/items#find"
-  get "/api/v1/items/find_all", to: "api/v1/items#find_all"
-  get "/api/v1/merchants/find", to: "api/v1/merchants#find"
-  get "/api/v1/merchants/find_all", to: "api/v1/merchants#find_all"
+  get "/api/v1/items/find", to: "api/v1/items#find" #find a single item which matches a search term
+  get "/api/v1/items/find_all", to: "api/v1/items#find_all" #find all items which match a search term
+  get "/api/v1/merchants/find", to: "api/v1/merchants#find" #find a single merchant which matches a search term
+  get "/api/v1/merchants/find_all", to: "api/v1/merchants#find_all" #find all merchants which match a search term
   post "/api/v1/items", to: "api/v1/items#create"
   post "/api/v1/merchants", to: "api/v1/merchants#create"
   put "/api/v1/items/:id", to: "api/v1/items#update"
