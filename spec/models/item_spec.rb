624a8c083ffbd720852858d6459b81d75ef25683
Jaren Garman <jarengarman@protonmail.com>
2025-02-28 19:30:20 -0500

    test: Organize class methods tests

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -6,17 +6,19 @@ describe Item, type: :model do
     it { is_expected.to belong_to :merchant }
   end
 
-  describe "sorted_by_price" do
-    it "can return a list of items sorted by price" do
-      create(:item, unit_price: 10)
-      create(:item, unit_price: 2)
-      create(:item, unit_price: 7)
-      create(:item, unit_price: 4)
-      create(:item, unit_price: 100)
+  describe "class methods" do
+    describe ".sorted_by_price" do
+      it "can return a list of items sorted by price" do
+        create(:item, unit_price: 10)
+        create(:item, unit_price: 2)
+        create(:item, unit_price: 7)
+        create(:item, unit_price: 4)
+        create(:item, unit_price: 100)
 
-      sorted_prices = Item.sort_by_price.pluck(:unit_price)
+        sorted_prices = Item.sort_by_price.pluck(:unit_price)
 
-      expect(sorted_prices).to eq([2, 4, 7, 10, 100])
+        expect(sorted_prices).to eq([2, 4, 7, 10, 100])
+      end
     end
   end
 end
--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -6,40 +6,42 @@ describe Merchant, type: :model do
     it { is_expected.to have_many :items }
   end
 
-  describe ".find_by_name_fragment" do
-    it "finds a single merchant by a name fragment" do
-      merchant1 = create(:merchant, name: "Logan's Store")
-      merchant2 = create(:merchant, name: "Alec's Store")
-      create(:merchant, name: "Logan's Shop")
-      create(:merchant, name: "Alec's Shop")
-      create(:merchant, name: "Dummy Merchant 1")
+  describe "class methods" do
+    describe ".with_returned_items" do
+      it "can return a list of merchants with returned items" do
+        create_list(:merchant, 5)
+        create(:invoice, status: "returned")
 
-      expect(Merchant.find_by_name_fragment("Logan")).to eq(merchant1)
-      expect(Merchant.find_by_name_fragment("Alec")).to eq(merchant2)
+        expect(Merchant.with_returned_items.count).to eq(1)
+      end
     end
-  end
 
-  describe "with_returned_items" do
-    it "can return a list of merchants with returned items" do
-      create_list(:merchant, 5)
-      create(:invoice, status: "returned")
+    describe ".find_by_name_fragment" do
+      it "finds a single merchant by a name fragment" do
+        merchant1 = create(:merchant, name: "Logan's Store")
+        merchant2 = create(:merchant, name: "Alec's Store")
+        create(:merchant, name: "Logan's Shop")
+        create(:merchant, name: "Alec's Shop")
+        create(:merchant, name: "Dummy Merchant 1")
 
-      expect(Merchant.with_returned_items.count).to eq(1)
+        expect(Merchant.find_by_name_fragment("Logan")).to eq(merchant1)
+        expect(Merchant.find_by_name_fragment("Alec")).to eq(merchant2)
+      end
     end
-  end
 
-  describe "with_item_count" do
-    it "can add an item count to indexed merchants" do
-      create(:merchant, id: 1)
-      create(:merchant, id: 2)
-      create(:merchant, id: 3)
-      create(:item, merchant_id: 1)
-      create(:item, merchant_id: 1)
-      create(:item, merchant_id: 2)
+    describe ".with_item_count" do
+      it "can add an item count to indexed merchants" do
+        create(:merchant, id: 1)
+        create(:merchant, id: 2)
+        create(:merchant, id: 3)
+        create(:item, merchant_id: 1)
+        create(:item, merchant_id: 1)
+        create(:item, merchant_id: 2)
 
-      expect(Merchant.with_item_count[0].item_count).to eq(1)
-      expect(Merchant.with_item_count[1].item_count).to eq(0)
-      expect(Merchant.with_item_count[2].item_count).to eq(2)
+        expect(Merchant.with_item_count[0].item_count).to eq(1)
+        expect(Merchant.with_item_count[1].item_count).to eq(0)
+        expect(Merchant.with_item_count[2].item_count).to eq(2)
+      end
     end
   end
 end
