e5c9c591581c3bea0d71dd51a546539d25b5be0f
Jaren Garman <jarengarman@protonmail.com>
2025-02-28 19:50:38 -0500

    feat: Add error handling for 404

--- app/controllers/api/v1/merchants/customers_controller.rb
+++ app/controllers/api/v1/merchants/customers_controller.rb
@@ -1,6 +1,13 @@
 class Api::V1::Merchants::CustomersController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found_response
   def index
     merchant = Merchant.find(params[:merchant_id])
     render json: CustomerSerializer.new(merchant.customers)
+  end
+
+  private
+
+  def not_found_response(e)
+    render json: ErrorSerializer.format_error(e, "404"), status: :not_found
   end
 end
