ae148d78b1c54db79de942b614ed578a7f6a9898
Jaren Garman <jarengarman@protonmail.com>
2025-03-01 21:20:14 -0500

    style: Remove explicit Merchant call to maintain consistency

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -5,7 +5,7 @@ class Merchant < ApplicationRecord
   has_many :customers, -> { distinct }, through: :invoices
 
   def self.sort_by_age
-    Merchant.order(created_at: :desc)
+    order(created_at: :desc)
   end
 
   def self.with_returned_items
@@ -15,11 +15,11 @@ class Merchant < ApplicationRecord
   end
 
   def self.find_by_name_fragment(fragment)
-    Merchant.where("name ILIKE ?", "%#{fragment}%").first
+    where("name ILIKE ?", "%#{fragment}%").first
   end
 
   def self.with_item_count
-    left_joins(:items)
+    joins(:items)
       .select("merchants.id, merchants.name, COUNT(items.id) AS item_count")
       .group("merchants.id, merchants.name")
   end
