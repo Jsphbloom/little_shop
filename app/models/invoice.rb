79a98381224d5c36051dc680ac8c56c917197903
Jaren Garman <jarengarman@protonmail.com>
2025-03-03 14:33:11 -0500

    refactor: Extract .filter_by_status to model

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -3,4 +3,8 @@ class Invoice < ApplicationRecord
   belongs_to :merchant
   has_many :transactions
   has_many :invoice_items
+
+  def self.filter_by_status(status)
+    where("status = '#{status}'")
+  end
 end
